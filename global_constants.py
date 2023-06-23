340e1360d50c151a2c4774fc9f215f48af507d95
nadiand <nadetodobreva@gmail.com>
2023-06-23 21:29:06 +0200

    Clean up initial transformer implementation

--- global_constants.py
+++ global_constants.py
@@ -1,20 +1,23 @@
 # data
+DIM = 2 # the dimensionality of the data
 NR_DETECTORS = 5
 EVENTS = 5000
 MAX_NR_TRACKS = 20
 NOISE_STD = 0.1
 HITS_DATA_PATH = 'hits_dataframe.csv'
 TRACKS_DATA_PATH = 'tracks_dataframe.csv'
+TEST_SPLIT = 0.2
+TRAIN_SPLIT = 0.9
 
 # transformer
-DIM = 2 # the dimensionality of the data
 NUM_EPOCHS = 1
 BATCH_SIZE = 128
+TEST_BATCH_SIZE = 1
 INPUT_SIZE = 3
-OUTPUT_SIZE = 3
-D_MODEL = 32
+OUTPUT_SIZE = 20
+D_MODEL = 64
 N_HEAD = 4
-DIM_FEEDFORWARD = 128
+DIM_FEEDFORWARD = 1
 NUM_ENCODER_LAYERS = 4
 LEARNING_RATE = 1e-4
 BETA1 = 0.9
@@ -22,4 +25,4 @@ BETA2 = 0.98
 EPS = 1e-9
 EARLY_STOPPING = 30
 PAD_TOKEN = 37 # just a random number outside of the range (-pi,pi) so it does not confuse the model
-TEST_BATCH_SIZE = 1
+PAD_LEN_DATA = MAX_NR_TRACKS * NR_DETECTORS
--- transformer.py
+++ transformer.py
@@ -2,8 +2,6 @@
 Transformer implementation.
 Taken from https://github.com/saulam/trajectory_fitting/tree/main
 """
-from typing import Optional
-
 import torch
 from torch import Tensor
 import torch.nn as nn
@@ -27,7 +25,6 @@ class FittingTransformer(nn.Module):
                                                  dropout=dropout)
         self.transformer_encoder = TransformerEncoder(encoder_layers, num_encoder_layers)
         self.proj_input = nn.Linear(input_size, d_model)
-        self.aggregator = nn.Linear(seq_len, 1)
         self.decoder = nn.Linear(d_model, output_size)
         self.dropout = nn.Dropout(dropout)
         self.init_weights()
@@ -39,27 +36,15 @@ class FittingTransformer(nn.Module):
         self.decoder.bias.data.zero_()
         self.decoder.weight.data.uniform_(-init_range, init_range)
 
-    def forward(self,
-                x: Tensor,  # Separate x tensor
-                y: Tensor,  # Separate y tensor
-                z: Optional[Tensor] = None,  # Separate z tensor; works with None
-                ):
-        # Initialize a tensor of zeros with the same size as the x and y tensors when z is None
-        if z is None:
-            z = torch.zeros_like(x)
-
-        # Combine x, y, and z tensors into coords tensor of shape (batch_size, sequence_length, 3)
-        coords = torch.stack((x, y, z), dim=2)
-
+    def forward(self, input: Tensor):
         # Linear projection of the input
-        src_emb = self.proj_input(coords)
+        src_emb = self.proj_input(input)
         # Transformer encoder
         memory = self.transformer_encoder(src=src_emb)
-        memory = self.aggregator(memory.permute(0, 2, 1))
-        memory = memory.squeeze(dim=2)
+        memory = torch.mean(memory, dim=0)
         # Dropout
         memory = self.dropout(memory)
         # Linear projection of the output
-        output = self.decoder(memory) #+ coords[:, :, :3]  # Learn residuals for x, y, z
+        output = self.decoder(memory)
 
         return output

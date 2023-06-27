082f6b1d09c393c789cfdb72897003b58f4b49e0
nadiand <nadetodobreva@gmail.com>
2023-06-27 22:12:14 +0200

    Implement visualization of 3d data

--- data_generation.py
+++ data_generation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from skspatial.objects import Line, Circle, Sphere
 from global_constants import *
 
@@ -11,26 +12,34 @@ def intersection(radius, pt1, pt2):
     intersection_point, _ = detector.intersect_line(line)
     return intersection_point
 
+
 def visualize_hits(hits_df):
     if DIM == 2:
-        ax = plt.gca()
-        for d in range(NR_DETECTORS+1):
-            circle = plt.Circle((0,0), d, alpha=0.5, fill=False)
-            ax.add_artist(circle)
-            ax.set_ylim(-7,7)
-            ax.set_xlim(-7,7)
+        fig, ax = plt.subplots()
+        ax.set_ylim(-6,6)
+        ax.set_xlim(-6,6)
+        for d in np.arange(1, NR_DETECTORS+1):
+            circle = Circle([0,0], d)
+            circle.plot_2d(ax, fill=False)
+    else: # dim==3
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        for d in np.arange(1, NR_DETECTORS+1):
+            sphere = Sphere([0,0,0], d)
+            sphere.plot_3d(ax, alpha=0.1)
 
-        for i in range(5,101,20):
-            row = hits_df.iloc[i]
-            for i in range(0, len(row), 3):
-                plt.plot(row[i], row[i + 1], marker="o", markerfacecolor="black", markeredgecolor="black")
+    for ind in range(5):
+        row = hits_df.iloc[ind]
+        for i in range(0, len(row), DIM+1):
+            if DIM == 2:
+                plt.plot(row[i], row[i+1], marker="o", markerfacecolor="black", markeredgecolor="black")
+            else: 
+                plt.plot(row[i], row[i+1], row[i+2], marker="o", markerfacecolor="black", markeredgecolor="black")
+    
+    plt.title("Visualization of a few events")
+    plt.savefig('hits_visualized.png')
+    plt.show()
 
-        plt.title("Visualization of a few events")
-        plt.savefig('hits_visualized.png')
-        plt.show()
-    else:
-        pass
-        # TODO
 
 def visualize_track_distribution(tracks_df):
     # Same for 2D and 3D data
@@ -44,6 +53,7 @@ def visualize_track_distribution(tracks_df):
     plt.hist(tracks)
     plt.show()
     plt.savefig('distr_tracks.png')
+
 
 if __name__ == '__main__':
     data = []
@@ -74,7 +84,6 @@ if __name__ == '__main__':
                 origin = [0,0] if DIM == 2 else [0,0,0]
                 point2 = [x,y] if DIM == 2 else [x,y,z]
                 intersection_point = intersection(d, origin, point2) 
-                # intersection = circle_intersection(d, [0,0], [x,y]) if DIM == 2 else sphere_intersection(d, [0,0,0], [x,y,z])
                 for coord in intersection_point:
                     row.append(coord + np.random.normal(0, NOISE_STD, 1)[0])
                 row.append(t*NR_DETECTORS+ind+1) # hit1trackid1
@@ -85,7 +94,7 @@ if __name__ == '__main__':
     hits_df = pd.DataFrame(data)
     tracks_df = pd.DataFrame(track_params)
     visualize_hits(hits_df)
-    visualize_track_distribution(tracks_df)
+    # visualize_track_distribution(tracks_df)
     hits_df.to_csv(HITS_DATA_PATH, header=None, index=False)
     tracks_df.to_csv(TRACKS_DATA_PATH, header=None, index=False)
     print("Data generated successfully!")

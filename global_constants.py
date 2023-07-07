357b02d173debac8f5a09f314dd457be69407571
nadiand <nadetodobreva@gmail.com>
2023-07-07 10:02:13 +0200

    Save current constants

--- global_constants.py
+++ global_constants.py
@@ -2,24 +2,23 @@
 DIM = 2 # the dimensionality of the data
 NR_DETECTORS = 5
 EVENTS = 5000
-MAX_NR_TRACKS = 20
+MAX_NR_TRACKS = 3 #20 # 3 for the simple regressor and 20 for the biger challenge
 NOISE_STD = 0.1
-HITS_DATA_PATH = 'hits_dataframe.csv'
-TRACKS_DATA_PATH = 'tracks_dataframe.csv'
+HITS_DATA_PATH = 'hits_dataframe.csv' #'C:/Users/lenovo/Desktop/uni/MLIPPA/Practical/particle_trajectory_prediction/hits_dataframe.csv'
+TRACKS_DATA_PATH = 'tracks_dataframe.csv' #'C:/Users/lenovo/Desktop/uni/MLIPPA/Practical/particle_trajectory_prediction/tracks_dataframe.csv'
 TEST_SPLIT = 0.2 # amount from the full dataset
 TRAIN_SPLIT = 0.9 # amount from the dataset left over after the test split is set aside
 
 # training
-NUM_EPOCHS = 3
+NUM_EPOCHS = 5
 BATCH_SIZE = 32
 TEST_BATCH_SIZE = 1
 EARLY_STOPPING = 30
 PAD_TOKEN = 101 # just a random number outside of the range (-pi,pi) and not possible to be a label number so it does not confuse the model
-PAD_LEN_DATA = MAX_NR_TRACKS * NR_DETECTORS
+MAX_LEN_DATA = MAX_NR_TRACKS * NR_DETECTORS
 
-# transformer
-INPUT_SIZE = 3
-OUTPUT_SIZE = 20
+INPUT_SIZE = 2
+OUTPUT_SIZE = 3#20 # 3 for the transformer (hits -> trajectories)
 D_MODEL = 64
 N_HEAD = 4
 DIM_FEEDFORWARD = 1

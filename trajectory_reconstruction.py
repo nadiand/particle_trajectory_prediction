0c566f7ec314ce2490464012b2ef85165a986608
nadiand <nadetodobreva@gmail.com>
2023-07-10 22:16:58 +0200

    Make RNN work with var len data and clean up code

--- trajectory_reconstruction.py
+++ trajectory_reconstruction.py
@@ -14,30 +14,52 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def predict_angle(rnn, clusters):
+    '''
+    Regresses the trajectory parameters based on the clusters of hits. Takes a set of
+    *clusters*, pads them up to 5 hits each if necessary, and passes them on to the 
+    *rnn* model. Directly returns the predictions.
+    '''
     padded_clusters, lens = [], []
     for cluster in clusters:
-        pad = [PAD_TOKEN, PAD_TOKEN] if DIM == 2 else [PAD_TOKEN, PAD_TOKEN, PAD_TOKEN]
+        # Keep track of real (unpadded) sizes of clusters, to pass on to the RNN
         lens.append(len(cluster))
-        padding = [pad for i in range(NR_DETECTORS-len(cluster))]
+
+        # Make padding based on how many hits each cluster is missing
+        pad = [PAD_TOKEN, PAD_TOKEN] if DIM == 2 else [PAD_TOKEN, PAD_TOKEN, PAD_TOKEN]
+        padding = [pad for _ in range(NR_DETECTORS-len(cluster))]
+
         padding = torch.tensor(padding).float()
+        if isinstance(cluster, list):
+            cluster = torch.tensor(cluster).float()
+        
+        # Pad the clusters with pseudo hits
         padded_clusters.append(torch.cat((cluster,padding),0))
     padded_clusters = torch.stack(padded_clusters)
+
+    # Make a prediction and return it
     pred = rnn(torch.tensor(padded_clusters).float(), torch.tensor(lens).int())
     return pred if DIM == 2 else torch.stack((pred[0],pred[1]),dim=1)
 
 
 def train(rnn, optim, train_loader, loss_fn):
+    '''
+    Conducts a single epoch of training: prediction, loss calculation, and loss
+    backpropagation. Returns the average loss over the whole train data.
+    '''
+    # Get the network in train mode
     torch.set_grad_enabled(True)
     rnn.train()
     losses = 0.
     n_batches = int(math.ceil(len(train_loader.dataset) / BATCH_SIZE))
     t = tqdm.tqdm(enumerate(train_loader), total=n_batches, disable=DISABLE_TQDM)
-    for i, data in t:
+    for _, data in t:
         _, group, label = data
+
         optim.zero_grad()
+        # Make prediction
         pred = predict_angle(rnn, group)
+        # Calculate loss and use it to update weights
         loss = loss_fn(pred, label)
-        print(pred, label, loss) # in label there are PAD_TOKENS so the loss is in the 1000s TODO
         loss.backward()
         optim.step()
         losses += loss.item()
@@ -47,14 +69,22 @@ def train(rnn, optim, train_loader, loss_fn):
 
 
 def evaluation(rnn, val_loader, loss_fn):
+    '''
+    Evaluates the network on the validation data by making a prediction and
+    calculating the loss. Returns the average loss over the whole val data.
+    '''
+    # Get the network in evaluation mode
     rnn.eval()
     losses = 0.
     n_batches = int(math.ceil(len(val_loader.dataset) / BATCH_SIZE))
     t = tqdm.tqdm(enumerate(val_loader), total=n_batches, disable=DISABLE_TQDM)
     with torch.no_grad():
-        for i, data in t:
+        for _, data in t:
             _, group, label = data
+
+            # Make prediction
             pred = predict_angle(rnn, group)
+            # Calculate loss
             loss = loss_fn(pred, label)
             losses += loss.item()
             t.set_description("loss = %.8f" % loss.item())
@@ -63,15 +93,20 @@ def evaluation(rnn, val_loader, loss_fn):
 
 
 def prediction(rnn, test_loader):
+    '''
+    Evaluates the network on the test data. Returns the predictions
+    '''
+    # Get the network in evaluation mode
     rnn.eval()
     predictions = {}
     n_batches = int(math.ceil(len(test_loader.dataset) / BATCH_SIZE))
     t = tqdm.tqdm(enumerate(test_loader), total=n_batches, disable=DISABLE_TQDM)
     with torch.no_grad():
-        for i, data in t:
+        for _, data in t:
             event_id, group, label = data
+            # Make prediction and add it to the list
             pred = predict_angle(rnn, group)
-            predictions[event_id] = (pred,label)#TODO remove the label from this function
+            predictions[event_id] = (pred,label)
     return predictions
 
 
@@ -90,13 +125,16 @@ def save_model(model, optim, type, val_losses, train_losses, epoch, count):
 if __name__ == '__main__':
     torch.manual_seed(37)  # for reproducibility
 
-    # load and split dataset into training, validation and test sets
+    # Load and split dataset into training, validation and test sets
     hits = pd.read_csv(HITS_DATA_PATH, header=None)
     tracks = pd.read_csv(TRACKS_DATA_PATH, header=None)
     dataset = GroupsDataset(hits, True, tracks)
     train_loader, valid_loader, test_loader = get_dataloaders(dataset)
 
+    # RNN model
     rnn = RNNModel(DIM, HIDDEN_SIZE_RNN, OUTPUT_SIZE_RNN)
+    pytorch_total_params = sum(p.numel() for p in rnn.parameters() if p.requires_grad)
+    print("Total trainable params: {}".format(pytorch_total_params))
     optim = torch.optim.Adam(rnn.parameters(), lr=RNN_LEARNING_RATE)
     loss_fn = nn.MSELoss()
 
@@ -104,7 +142,9 @@ if __name__ == '__main__':
     min_val_loss = np.inf
     count = 0
     for epoch in range(NUM_EPOCHS):
+        # Train the model
         train_loss = train(rnn, optim, train_loader, loss_fn)
+        # Evaluate on validation data
         val_loss = evaluation(rnn, valid_loader, loss_fn)
         print((f"Epoch: {epoch}, Train loss: {train_loss:.8f}, Val loss: {val_loss:.8f}"))
 
@@ -112,10 +152,12 @@ if __name__ == '__main__':
         val_losses.append(val_loss)
 
         if val_loss < min_val_loss:
+            # If the model has a new best validation loss, save it as "the best"
             min_val_loss = val_loss
             save_model(rnn, optim, "best", val_losses, train_losses, epoch, count)
             count = 0
         else:
+            # If the model's validation loss isn't better than the best, save it as "the last"
             save_model(rnn, optim, "last", val_losses, train_losses, epoch, count)
             count += 1
 
@@ -123,6 +165,7 @@ if __name__ == '__main__':
         #     print("Early stopping...")
         #     break
     
+    # Predict on the test data
     # print(prediction(rnn, test_loader))
 
 
